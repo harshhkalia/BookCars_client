35314085c8123ff3621ad98d9509d6ad80213b31
Harsh Kalia <kaliaharsh24@gmail.com>
2025-07-05 00:01:18 +0530

    Add lazy loading and vercel caching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
 import ProtectedRoute from "./protectedRoute";
-import Auth from "./components/auth/Auth";
 
+// Lazy load components
+const Auth = lazy(() => import("./components/auth/Auth"));
 const CustomerHome = lazy(() => import("./components/home/CustomerHome"));
 const OwnerInitialHP = lazy(() => import("./components/home/OwnerInitialHP"));
 const OwnerHome = lazy(() => import("./components/home/OwnerHome"));
